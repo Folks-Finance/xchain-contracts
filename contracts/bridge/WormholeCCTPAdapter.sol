11a8f65154dfb31f9ad27931eead95ef98c09509
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-02-21 13:12:12 +0200

    Update WormholeCCTPAdapter.sol

--- contracts/bridge/WormholeCCTPAdapter.sol
+++ contracts/bridge/WormholeCCTPAdapter.sol
@@ -59,7 +59,7 @@ contract WormholeCCTPAdapter is IBridgeAdapter, IWormholeReceiver, AccessControl
 
     /**
      * @notice Constructor
-     * @param admin The default admin for AcccountManager
+     * @param admin The default admin for AccountManager
      * @param _wormhole The Wormhole Core to get message fees
      * @param _wormholeRelayer The Wormhole Relayer to relay messages using
      * @param _bridgeRouter The Bridge Router to route messages through
